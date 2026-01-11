bca6cb7b277d95c75a716253f90dd4e9d1604288
Zainab Ali Mousa <safaali8118-email@example.com>
2026-01-11 23:49:08 +0200

    edited

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import QuranHeart from "@/components/QuranHeart";
 
 export default function Home() {
   return <>
-    <main className="flex justify-center items-center py-2 bg-gray-100 min-h-screen">
-      <QuranHeart />
-    </main>
+  <main className="flex flex-col justify-center items-center py-6 bg-gray-50 min-h-screen">
+  <h1 className="text-2xl py-2 font-semibold text-gray-800 text-center">
+    وَرَتِّلِ الْقُرْآنَ تَرْتِيلًا 💜
+  </h1>
+ 
+  <QuranHeart />
+   <p className="mt-2 text-gray-600 text-center max-w-md">
+    عَنِ النَّبِيِّ ﷺ: <br/>
+    «خَيْرُكُمْ مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ»
+  </p>
+</main>
+
   </>;
 }
